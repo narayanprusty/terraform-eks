05d25547b9fa3a4bb2c18891d72e5c3a2607c17d
Narayan Prusty <arsenal.narayan@gmail.com>
2019-09-15 21:35:21 +0100

    removed private subnets

--- local.tf
+++ local.tf
@@ -7,7 +7,6 @@ locals {
   vpc_cidr        = "10.0.0.0/16"
   azs             = ["us-east-1b", "us-east-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 
   image_id      = "ami-0c5b63ec54dd3fc38"
   instance_type = "t2.medium"
